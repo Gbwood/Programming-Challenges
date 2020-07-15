29a90cbc3c18463194ea69aed87644622ab5b003
Graham Wood <gbwood@ksu.edu>
2020-07-15 09:28:27 -0500

    BinaryTreeFindCommonAncestor

---
+++ Trees/FirstCommonAncestor.cs
@@ -0,0 +1,37 @@
+	//1. Find depth of each
+	//2. Go up tree on deeper node until equal
+	//3. Linearly go up tree until they equal same node
+
+
+
+public Node FindCommon(Node A, Node B)
+{
+	Int depthA = findDepth(A);
+	Int depthB = findDepth(B);
+
+	if(depthA > depthB) {
+		while (depthA > depthB)
+		{
+			A = A.parent;
+			depthA--;
+		}
+	}
+	else if (depthB > depthA) {
+		while (depthB > depthA)
+		{
+			B = B.parent;
+			DepthB--;
+		}
+
+	}
+
+	while(A != B)
+	{
+		A = A.parent;
+		B = B.parent;
+	}
+
+	Return A;
+
+
+}
