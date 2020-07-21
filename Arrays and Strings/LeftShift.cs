e9c16362b8d028c3a006b21b2925dc274ccf2145
Graham Wood <gbwood@ksu.edu>
2020-07-20 23:08:17 -0500

    Left shift

---
+++ Arrays and Strings/LeftShift.cs
@@ -0,0 +1,46 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+
+
+    static void Main(string[] args)
+    {
+        string[] nd = Console.ReadLine().Split(' ');
+
+        int n = Convert.ToInt32(nd[0]);
+
+        int d = Convert.ToInt32(nd[1]);
+
+        int[] a = Array.ConvertAll(Console.ReadLine().Split(' '), aTemp => Convert.ToInt32(aTemp))
+        ;
+
+
+        int[] newArray = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            int index = i + d;
+            if (index > n - 1)
+            {
+                index = index - n;
+            }
+            Console.Write(a[index] + " ");
+        }
+
+
+    }
+}
