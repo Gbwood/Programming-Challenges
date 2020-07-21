734da87f08ee83ded257f62d816338a489a32b93
Graham Wood <gbwood@ksu.edu>
2020-07-21 01:50:23 -0500

    Linked lists

--- LinkedLists/SimpleRecursivePrint.cs
+++ LinkedLists/SimpleRecursivePrint.cs
@@ -1,3 +1,5 @@
+
+//iterate with recursion
 static void printLinkedList(SinglyLinkedListNode head)
 {
     Console.WriteLine(head.data);
@@ -5,5 +7,22 @@ static void printLinkedList(SinglyLinkedListNode head)
     {
         printLinkedList(head.next);
     }
+
+}
+
+
+//add at end
+static SinglyLinkedListNode insertNodeAtTail(SinglyLinkedListNode head, int data)
+{
+    SinglyLinkedListNode runner = head;
+
+    while (runner.next != null)
+    {
+        runner = runner.next;
+    }
+
+    SinglyLinkedListNode tail = new SinglyLinkedListNode(data);
+    runner.next = tail;
+    return head;
 
 }
